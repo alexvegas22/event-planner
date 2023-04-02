a06d260590421fe67c03827e1fef1b1b74942e09
mmalo <1542354@crosemont.qc.ca>
2023-04-01 20:45:51 -0400

    base de données révisée + ajout de diagramme

--- src/sql/planner.sql
+++ src/sql/planner.sql
@@ -1,57 +1,116 @@
+-- MySQL Workbench Forward Engineering
+
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE=ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE=`ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION`;
 
 -- -----------------------------------------------------
 -- Schema mydb
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema projetsession
+-- Schema planner
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS projetsession DEFAULT CHARACTER SET utf8mb4 COLLATE = utf8mb4_unicode_ci ;
-USE projetsession ;
+CREATE SCHEMA IF NOT EXISTS `planner` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `planner` ;
 
 -- -----------------------------------------------------
--- Table projetsession.utilisateurs
+-- Table `planner`.`Administrateur`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS projetsession.utilisateurs (
-  id INT NOT NULL AUTO_INCREMENT,
-  email VARCHAR(150) NOT NULL,
-  nom VARCHAR(25) NOT NULL,
-  password VARCHAR(25) NOT NULL,
-  photo VARCHAR(150) NULL,
-  prenom VARCHAR(25) NOT NULL,
-  PRIMARY KEY (id),
-  UNIQUE INDEX id_UNIQUE (id ASC) )
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table projetsession.evenements
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS projetsession.evenements (
-  id INT NOT NULL AUTO_INCREMENT,
-  nom VARCHAR(50) NOT NULL,
-  lieux VARCHAR(100) NOT NULL,
-  debut date NOT NULL,
-  fin date NOT NULL,
-  description LONGTEXT NULL,
-  PRIMARY KEY (id),
-  UNIQUE INDEX id_UNIQUE (id ASC))
+CREATE TABLE IF NOT EXISTS `planner`.`Administrateur` (
+  `idAdmin` INT NOT NULL AUTO_INCREMENT,
+  `nomAdmin` VARCHAR(20) NOT NULL,
+  `prenomAdmin` VARCHAR(20) NOT NULL,
+  `emailAdmin` VARCHAR(255) NOT NULL,
+  `passwordAdmin` VARCHAR(20) NOT NULL,
+  PRIMARY KEY (`idAdmin`),
+  UNIQUE INDEX `idAdmin_UNIQUE` (`idAdmin` ASC))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 -- -----------------------------------------------------
--- Table projetsession.utilisateurs_evenements
+-- Table `planner`.`Utilisateur`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS projetsession.utilisateurs_evenements (
-  id INT NOT NULL,
-  id INT NOT NULL,
-  PRIMARY KEY (id, id),
-  FOREIGN KEY (id) REFERENCES projetsession.utilisateurs (id),
-  FOREIGN KEY (id) REFERENCES projetsession.evenements (id))
+CREATE TABLE IF NOT EXISTS `planner`.`Utilisateur` (
+  `idUser` INT NOT NULL AUTO_INCREMENT,
+  `nomUser` VARCHAR(20) NOT NULL,
+  `prenomUser` VARCHAR(20) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `telephone` VARCHAR(10),
+  `password` VARCHAR(20) NOT NULL,
+  `bio` LONGTEXT,
+  `photoProfil` VARCHAR(20) NOT NULL,
+  PRIMARY KEY (`idUser`),
+  UNIQUE INDEX `idUser_UNIQUE` (`idUser` ASC))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+
+-- -----------------------------------------------------
+-- Table `planner`.`Organisateur`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `planner`.`Organisateur` (
+  `idUser` INT NOT NULL,
+  `idEvent` INT NOT NULL,
+  PRIMARY KEY (`idUser`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+
+-- -----------------------------------------------------
+-- Table `planner`.`Participant`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `planner`.`Participant` (
+  `idUser` INT NOT NULL,
+  `idEvent` INT NOT NULL,
+  PRIMARY KEY (`idUser`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+
+-- -----------------------------------------------------
+-- Table `planner`.`Evenement`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `planner`.`Evenement` (
+  `idEvent` INT NOT NULL AUTO_INCREMENT,
+  `nomEvent` VARCHAR(45) NOT NULL,
+  `lieu` VARCHAR(45) NOT NULL,
+  `debut` VARCHAR(45) NOT NULL,
+  `fin` VARCHAR(45) NOT NULL,
+  `description` LONGTEXT,
+  `photoEvent` VARCHAR(20),
+  `categorie` VARCHAR(20) NOT NULL,
+  `visibilite` VARCHAR(20) NOT NULL,
+  `idVisibilite` INT NOT NULL,
+  PRIMARY KEY (`idEvent`),
+  UNIQUE INDEX `idEvent_UNIQUE` (`idEvent` ASC))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+  
+-- -----------------------------------------------------
+-- Table `planner`.`Public`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `planner`.`Public` (
+  `idVisibilite` INT NOT NULL,
+  PRIMARY KEY (`idVisibilite`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+
+-- -----------------------------------------------------
+-- Table `planner`.`Prive`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `planner`.`Prive` (
+  `idVisibilite` INT NOT NULL,
+  PRIMARY KEY (`idVisibilite`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+
+-- -----------------------------------------------------
+-- Table `planner`.`ToDo`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `planner`.`ToDo` (
+  `idTodo` INT NOT NULL AUTO_INCREMENT,
+  `nomTodo` VARCHAR(45) NULL,
+  `date` VARCHAR(45) NULL,
+  PRIMARY KEY (`idTodo`),
+  UNIQUE INDEX idTodo_UNIQUE (idTodo ASC))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
@@ -59,23 +118,3 @@ DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-
-INSERT INTO projetsession.utilisateurs
-(id, email, nom, prenom, password, photoProfil)
-VALUES
-(1,'aimen@gmail.com','aimen','essaie','aimen123','essaie.png'),
-(2,'alex@gmail.com','alex','exemple','alex123','exemple.png'),
-(3,'mely@gmail.com','mely','test','melyse123','test.png');
-
-
-
-
-INSERT INTO projetsession.evenements
-(id, nom, lieux, debut, fin, description)
-VALUES
-(1,'feteAimen', 'Montreal','2023-10-18 07:59:14','2023-10-18 20:00:14','La fête de Aimen'),
-(2,'MariageLola', 'Laval','2023-06-18 11:00:00','2023-06-18 22:00:00','Le mariage de Lola'),
-(3,'GraduationImed', 'Longueil','2023-06-20 12:59:14','2023-06-21 00:00:00','La graduation de Imed');
-
-
-INSERT INTO projetsession.utilisateurs_evenements VALUES (1,1),(2,1),(3,2);
