45034ecab0d85d51dad53cfb863c19db452077a9
Aimen <djeaimen@gmail.com>
2023-05-19 18:11:15 -0400

    recherche admin

--- src/java/com/controllers/controllerRechercheAdmin.java
+++ src/java/com/controllers/controllerRechercheAdmin.java
@@ -17,7 +17,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author djeai
  */
-public class rechercheAdmin extends HttpServlet {
+public class controllerRechercheAdmin extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
