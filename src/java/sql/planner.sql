30d4124ba83d551626829cb4fa9c373e1554ce85
Melyse <mmalo@dept-info.crosemont.quebec>
2023-05-25 10:23:02 -0400

    MAJ sql et mini changement

--- src/java/sql/planner.sql
+++ src/java/sql/planner.sql
@@ -2,7 +2,7 @@
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE=`ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION`;
 
 -- -----------------------------------------------------
 -- Schema mydb
@@ -11,28 +11,28 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema planner
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS 'planner' DEFAULT CHARACTER SET utf8mb4 ;
-USE 'planner' ;
+CREATE SCHEMA IF NOT EXISTS `planner` DEFAULT CHARACTER SET utf8mb4 ;
+USE `planner` ;
 
 -- -----------------------------------------------------
--- Table 'planner'.'utilisateur'
+-- Table `planner`.`utilisateur`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS 'planner'.'utilisateurs' (
-  'id' INT NOT NULL AUTO_INCREMENT,
-  'nom' VARCHAR(65) NOT NULL,
-  'prenom' VARCHAR(65) NOT NULL,
-  'email' VARCHAR(255) NOT NULL,
-  'telephone' VARCHAR(65),
-  'password' VARCHAR(65) NOT NULL,
-  'bio' LONGTEXT,
-  'photo' VARCHAR(255),
-  PRIMARY KEY ('id'),
-  UNIQUE INDEX 'id_UNIQUE' ('id' ASC))
+CREATE TABLE IF NOT EXISTS `planner`.`utilisateurs` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nom` VARCHAR(65) NOT NULL,
+  `prenom` VARCHAR(65) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `telephone` VARCHAR(65),
+  `password` VARCHAR(65) NOT NULL,
+  `bio` LONGTEXT,
+  `photo` VARCHAR(255),
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4 ;
 
 -- -----------------------------------------------------
--- Table 'planner'.'evenement'
+-- Table `planner`.`evenement`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `planner`.`evenements` (
 `id` INT NOT NULL auto_increment,
@@ -49,14 +49,14 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4 ;
 
 -- -----------------------------------------------------
--- Table 'planner'.'todo'
+-- Table `planner`.`todo`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS 'planner'.'todo' (
-  'id' INT NOT NULL AUTO_INCREMENT,
-  'tache' VARCHAR(255) NOT NULL,
-  'fait' BOOLEAN,
-  PRIMARY KEY ('id'),
-  UNIQUE INDEX 'id_UNIQUE' ('id' ASC))
+CREATE TABLE IF NOT EXISTS `planner`.`todo` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `tache` VARCHAR(255) NOT NULL,
+  `fait` BOOLEAN,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4 ;
 
